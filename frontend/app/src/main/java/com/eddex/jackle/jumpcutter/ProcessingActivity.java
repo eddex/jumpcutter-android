f57f3802c4f12334a03507e8486d37bee353e566
eddex <eddex@outlook.com>
2019-05-10 09:41:12 +0200

    add processing activity

---
+++ frontend/app/src/main/java/com/eddex/jackle/jumpcutter/ProcessingActivity.java
@@ -0,0 +1,46 @@
+package com.eddex.jackle.jumpcutter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ProcessingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+
+    /**
+     * Inflates (creates) menu
+     * @param menu activity menu which will be inflated
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * Occurs when menu item is clicked
+     * @param item clicked menu item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.myvideos:
+                this.startActivity(new Intent(this, MyVideosActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
