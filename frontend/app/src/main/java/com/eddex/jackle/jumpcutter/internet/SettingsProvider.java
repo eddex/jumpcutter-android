f1774d2e4b98681cc4d32be2adb2b258782b6643
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-05-10 18:02:21 +0200

    added settingsprovider

---
+++ frontend/app/src/main/java/com/eddex/jackle/jumpcutter/internet/SettingsProvider.java
@@ -0,0 +1,41 @@
+package com.eddex.jackle.jumpcutter.internet;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SettingsProvider {
+    SharedPreferences preferences;
+
+    public SettingsProvider(Context context) {
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String getSoundSpeed() {
+        return this.preferences.getString("list_preference_sound_speed", null);
+    }
+
+    public String geSilenceSpeed() {
+        return this.preferences.getString("list_preference_silence_speed", null);
+    }
+
+    public String geSilenceThreshold() {
+        return this.preferences.getString("list_preference_silence_threshold", null);
+    }
+
+    public Boolean getAdvancedOptionsSwitchEnabled() {
+        return this.preferences.getBoolean("advanced_options_switch", false);
+    }
+
+    public String getSampleRate() {
+        return this.preferences.getString("list_preference_sample_rate", null);
+    }
+
+    public String getFrameQuality() {
+        return this.preferences.getString("list_preference_frame_quality", null);
+    }
+
+    public String getFrameRate() {
+        return this.preferences.getString("list_preference_frame_rate", null);
+    }
+}
